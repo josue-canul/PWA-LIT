d2ee48796d2df2c1bcd8eb09a837d891c5d404d6
josue-canul <josue.19070015@itsmotul.edu.mx>
2023-02-08 23:34:10 -0600

    my-element.ts

--- intro to lit/my-element.ts
+++ intro to lit/my-element.ts
@@ -1,13 +1,18 @@
 import {LitElement, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 
-@customElement('my-element')
-export class MyElement extends LitElement {
-// TODO: Add a reactive property
+@customElement('name-tag')
+export class NameTag extends LitElement {
+  @property()
+  name: string = 'Your name here';
 
   render() {
+    // TODO: Add declarative event listener to input.
     return html`
-      <p>TODO: Add an expression</p>
+      <p>Hello, ${this.name}</p>
+      <input placeholder="Enter your name">
     `;
   }
+
+  // TODO: Add event handler method.
 }
