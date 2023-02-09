d941f213819b0791f86696902a5bf561358f88d5
josue-canul <josue.19070015@itsmotul.edu.mx>
2023-02-09 16:22:51 -0600

    my-element.ts

--- working with list/my-element.ts
+++ working with list/my-element.ts
@@ -1,39 +1,50 @@
-import {LitElement, html} from 'lit';
-import {customElement, state} from 'lit/decorators.js';
-import type {TemplateResult} from 'lit';
+import {html, css, LitElement} from 'lit';
+import {customElement} from 'lit/decorators.js';
+// TODO: Import directives
 
 @customElement('my-element')
 class MyElement extends LitElement {
-  @state()
-  friends = ['Harry', 'Ron', 'Hermione'];
+  static styles = css`
+    /* playground-fold */
+    :host {
+      display: block;
+      width: 400px;
+      height: 400px;
+    }
+    #board {
+      display: grid;
+      grid-template-columns: repeat(8, 1fr);
+      grid-template-rows: repeat(8, 1fr);
+      border: 2px solid #404040;
+      box-sizing: border-box;
+      height: 100%;
+    }
+    #board > div {
+      padding: 2px;
+    }
+    .black {
+      color: #ddd;
+      background: black;
+    }
+    .white {
+      color: gray;
+      background: white;
+    }
+    /* playground-fold-end */
 
-  @state()
-  pets = [
-    { name: "Hedwig", species: "Owl" },
-    { name: "Scabbers", species: "Rat" },
-    { name: "Crookshanks", species: "Cat" },
-  ];
-
-  @state()
-  includePets = true;
+  `;
 
   render() {
-    const listItems: TemplateResult[] = [];
-    // TODO: populate templates with items to render.
-
     return html`
-      <button @click=${() => this._togglePetVisibility()}>
-        ${this.includePets ? 'Hide' : 'Show'} pets
-      </button>
-      <p>My magical friends</p>
-      <ul>
-        <!-- TODO: Render templates. -->
-      </ul>
+      <p>Let's play a game!</p>
+      <div id="board">
+        <!-- TODO: Place squares here. -->
+      </div>
     `;
-  }
-
-  private _togglePetVisibility() {
-    this.includePets = !this.includePets;
   }
 }
 
+const getColor = (row: number, col: number) =>
+  (row + col) % 2 ? 'white' : 'black';
+const getLabel = (row: number, col: number) =>
+  `${String.fromCharCode(65 + col)}${8 - row}`;
