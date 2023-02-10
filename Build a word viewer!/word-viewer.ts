e58446625d4dff3c78d31e39880cad080f3ac2c0
josue-canul <josue.19070015@itsmotul.edu.mx>
2023-02-10 15:46:23 -0600

    word-viewer.ts

--- Build a word viewer!/word-viewer.ts
+++ Build a word viewer!/word-viewer.ts
@@ -3,13 +3,18 @@ import { customElement, property, state } from 'lit/decorators.js';
 
 @customElement('word-viewer')
 class WordViewer extends LitElement {
-  // TODO: Add `idx` state
+  @state() private idx = 0;
   @property() words = 'initial value';
 
+  // TODO: Define a `connectedCallback` which sets an interval.
+  // TODO: Define a `disconnectedCallback` which clears the interval.  
+
   render() {
-    // TODO: Split the `words` by `'.'`, and from the resulting word array
-    // only show the word on index `this.idx`.
-    return html`<pre>${this.words}</pre>`;
+    const splitWords = this.words.split('.');
+    const word = splitWords[this.idx % splitWords.length];
+    return html`<pre>${word}</pre>`;
   }
+
+  // TODO: Define `tickToNextWord`.
 }
 
