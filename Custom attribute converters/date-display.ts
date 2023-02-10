d6f1b510615917fc08ef6d7ac469adfbfc6c06be
josue-canul <josue.19070015@itsmotul.edu.mx>
2023-02-10 11:54:06 -0600

    date-display.ts

--- Custom attribute converters/date-display.ts
+++ Custom attribute converters/date-display.ts
@@ -1,19 +1,11 @@
-import {LitElement, html, PropertyValues} from 'lit';
+import {LitElement, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
+import {dateConverter} from './date-converter.js';
 
 @customElement('date-display')
 export class DateDisplay extends LitElement {
-  @property({attribute: false})
+  @property({converter: dateConverter(navigator.language), reflect: true})
   date = new Date();
-
-  @property({type: String, attribute: 'date-str'})
-  dateStr = '';
-
-  willUpdate(changed: PropertyValues<this>) {
-    if (changed.has('dateStr') && this.dateStr) {
-      this.date = new Date(this.dateStr);
-    }
-  }
 
   render() {
     const locale = 'en-US';
@@ -24,3 +16,4 @@ export class DateDisplay extends LitElement {
     `;
   }
 }
+
