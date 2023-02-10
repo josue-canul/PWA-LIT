93905fbb8fbc503766aa2905a50227a447884007
josue-canul <josue.19070015@itsmotul.edu.mx>
2023-02-10 15:36:40 -0600

    word-viewer.ts

--- Build a word viewer!/word-viewer.ts
+++ Build a word viewer!/word-viewer.ts
@@ -1,55 +1,14 @@
-import { css, html, LitElement } from 'lit';
-import { customElement, property, state } from 'lit/decorators.js';
-import { classMap } from 'lit/directives/class-map.js';
+import { html, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
 
-@customElement('word-viewer')
 class WordViewer extends LitElement {
-  static styles = css`
-    :host {
-      background-color: white;
-      color: violet;
-      cursor: pointer;
-      display: block;
-    }
-    pre {
-      padding: 0.2em;
-    }
-    .backwards {
-      color: white;
-      background-color: violet;
-    }
-  `
-
-  @state() private playDirection: -1 | 1 = 1;
-  @state() private idx = 0;
-  @property() words = 'initial value';
-
-  private intervalTimer?: number;
-
-  connectedCallback() {
-    super.connectedCallback();
-    this.intervalTimer = setInterval(this.tickToNextWord, 1000);
-  }
-
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    clearInterval(this.intervalTimer);
-    this.intervalTimer = undefined;
-  }
-
   render() {
-    const splitWords = this.words.split('.');
-    const idx = ((this.idx % splitWords.length) + splitWords.length) % splitWords.length;
-    const word = splitWords[idx];
-    return html`<pre
-      class="${classMap({ backwards: this.playDirection === -1 })}"
-      @click=${this.switchPlayDirection}
-    >${word}</pre>`;
+    // TODO: Render something!
   }
+}
 
-  tickToNextWord = () => { this.idx += this.playDirection; };
-
-  switchPlayDirection() {
-    this.playDirection *= -1;
+declare global {
+  interface HTMLElementTagNameMap {
+    // TODO: Add "word-viewer" as an HTML tag that TypeScript understands
   }
 }
