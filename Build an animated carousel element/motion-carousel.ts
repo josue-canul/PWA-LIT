662435811dc171a1ac0f05918cadead4f03e42ce
josue-canul <josue.19070015@itsmotul.edu.mx>
2023-02-10 12:58:08 -0600

    styles.ts

--- Build an animated carousel element/motion-carousel.ts
+++ Build an animated carousel element/motion-carousel.ts
@@ -23,10 +23,18 @@ export class MotionCarousel extends LitElement {
       this.selectedInternal = this.selected;
     }
     return html`
-      <div class="fit">
+      <div class="fit" @click=${this.clickHandler}>
         <slot name="selected"></slot>
       </div>
     `;
+  }
+
+  private clickHandler(e: MouseEvent) {
+    const i = this.selected + (Number(!e.shiftKey) || -1);
+    this.selected = i > this.maxSelected ? 0 : i < 0 ? this.maxSelected : i;
+    const change = new CustomEvent('change',
+      {detail: this.selected, bubbles: true, composed: true});
+    this.dispatchEvent(change);
   }
 
   private previous = 0;
