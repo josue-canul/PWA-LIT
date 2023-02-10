4a56f1dfd4d020644b143b71f231c3bc15dcf77f
josue-canul <josue.19070015@itsmotul.edu.mx>
2023-02-10 12:54:31 -0600

    styles.ts

--- Build an animated carousel element/motion-carousel.ts
+++ Build an animated carousel element/motion-carousel.ts
@@ -1,4 +1,4 @@
-import {LitElement, html} from 'lit';
+import {LitElement, html, PropertyValues} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import {styles} from './styles.js';
 
@@ -6,7 +6,22 @@ import {styles} from './styles.js';
 export class MotionCarousel extends LitElement {
   static styles = styles;
 
+  private selectedInternal = 0;
+  @property({type: Number})
+  selected = 0;
+
+  get maxSelected() {
+    return this.childElementCount - 1;
+  }
+
+  hasValidSelected() {
+    return this.selected >= 0 && this.selected <= this.maxSelected;
+  }
+
   render() {
+    if (this.hasValidSelected()) {
+      this.selectedInternal = this.selected;
+    }
     return html`
       <div class="fit">
         <slot></slot>
@@ -15,4 +30,3 @@ export class MotionCarousel extends LitElement {
   }
 
 }
-
